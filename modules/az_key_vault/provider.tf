27a3038942090d7e9dbb9d266ab4a97ded0e9d06
Luiz Henrique Arantes <79605326+lharantes@users.noreply.github.com>
2025-04-21 10:51:23 -0300

    Update provider.tf

--- modules/az_key_vault/provider.tf
+++ modules/az_key_vault/provider.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>4.4.0"
-    }
-  }
-  required_version = "=1.9.0"
-}
-
 provider "azurerm" {
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
