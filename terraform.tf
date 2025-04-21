7f9d0ef96364f2e4df44e4efc4db95e008097bf6
Luiz Henrique <luiz.arantes@live.com>
2025-04-21 06:37:18 -0300

    first commit

---
+++ terraform.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.9, < 2.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.117, < 5.0"
+    }
+        azuread = {
+      source = "hashicorp/azuread"
+      version = "3.3.0"
+        }
+  }
+}
+
